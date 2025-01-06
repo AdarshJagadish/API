8c25831c7fb6d4f06115120ed5c39f99336cec14
AdarshJagadish <jagadishadarsh2003@gmail.com>
2025-01-06 13:24:22 +0530

    initial commit

--- DRF/app/serializer.py
+++ DRF/app/serializer.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from .models import *
-class studSerializer(serializers.Serializer):
-    name=serializers.CharField()
-    age=serializers.IntegerField()
-    email=serializers.CharField()
+# class studSerializer(serializers.Serializer):
+#     name=serializers.CharField()
+#     age=serializers.IntegerField()
+#     email=serializers.CharField()
+    
+class studModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=stud
+        fields= '__all__'
--- DRF/app/views.py
+++ DRF/app/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render
 from django.http import JsonResponse,HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 from .serializer import *
 
-def fun1(request):
+# def fun1(request):
+#     if request.method=='GET':
+#         data = stud.objects.all()
+#         s=studSerializer(data,many=True)
+#         return JsonResponse(s.data,safe=False)
+    
+    
+# @csrf_exempt
+# def fun1(request):
+#     if request.method=='GET':
+#         data=stud.objects.all()
+#         s=studModelSerializer(data,many=True)
+#         return JsonResponse(s.data,safe=False)
+#     elif request.method=='POST':
+#         d=JSONParser().parse(request)
+#         s=studModelSerializer(data=d)
+#         print(s)
+#         if s.is_valid():
+#             s.save()
+#             return JsonResponse(s.data,safe=False)
+#         else:
+#             return JsonResponse(s.errors)
+        
+        
+@csrf_exempt
+def fun1(request,pk):
+    try:
+        demo=stud.objects.get(pk=pk)
+        print('helo')
+    except:
+        return HttpResponse("Invalid")
     if request.method=='GET':
-        data = stud.objects.all()
-        s=studSerializer(data,many=True)
-        return JsonResponse(s.data,safe=False)
+        s=studModelSerializer(demo)
+        return JsonResponse(s.data)
+    
 # Create your views here.
