66aa37661ee9f0a5286516cba29be2b106146d9f
AdarshJagadish <jagadishadarsh2003@gmail.com>
2025-01-08 13:35:10 +0530

    initial commit

--- DRF/app/urls.py
+++ DRF/app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('<pk>',views.fun1),
+    path('',views.fun1.as_view()),
 ]
--- DRF/app/views.py
+++ DRF/app/views.py
@@ -4,6 +4,20 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .serializer import *
 
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
+from rest_framework import generics,mixins
+
+
+#/-------------------------------------------------------------------------------------------------------\
+#     ******************************************** POST *********************************************
+#\-------------------------------------------------------------------------------------------------------/
+
+
 # def fun1(request):
 #     if request.method=='GET':
 #         data = stud.objects.all()
@@ -11,12 +25,17 @@ from .serializer import *
 #         return JsonResponse(s.data,safe=False)
     
     
+#/-------------------------------------------------------------------------------------------------------\
+#     ******************************************** POST *********************************************
+#\-------------------------------------------------------------------------------------------------------/
+
+
 # @csrf_exempt
 # def fun1(request):
 #     if request.method=='GET':
 #         data=stud.objects.all()
 #         s=studModelSerializer(data,many=True)
-#         return JsonResponse(s.data,safe=False)
+#         return JsonResponse(s.data,safe=False)                 
 #     elif request.method=='POST':
 #         d=JSONParser().parse(request)
 #         s=studModelSerializer(data=d)
@@ -27,16 +46,128 @@ from .serializer import *
 #         else:
 #             return JsonResponse(s.errors)
         
+
         
-@csrf_exempt
-def fun1(request,pk):
-    try:
-        demo=stud.objects.get(pk=pk)
-        print('helo')
-    except:
-        return HttpResponse("Invalid")
-    if request.method=='GET':
-        s=studModelSerializer(demo)
-        return JsonResponse(s.data)
+#/-------------------------------------------------------------------------------------------------------\
+#     ******************************************** PUT **********************************************
+#\-------------------------------------------------------------------------------------------------------/
+
+
+
+# @csrf_exempt
+# def fun1(request,pk):
+#     try:
+#         demo=stud.objects.get(pk=pk)
+#         # print('helo')
+#     except:
+#         return HttpResponse("Invalid")                      
+#     if request.method=='GET':                                      
+#         s=studModelSerializer(demo)                         
+#         return JsonResponse(s.data)
+#     elif request.method=='PUT':
+#         d=JSONParser().parse(request)
+#         s=studModelSerializer(demo,data=d)
+#         if s.is_valid():
+#             s.save()
+#             return JsonResponse(s.data)
+#         else:
+#             return JsonResponse(s.errors)
+
+
+#/-------------------------------------------------------------------------------------------------------\
+#     ******************************************** DELETE *********************************************
+#\-------------------------------------------------------------------------------------------------------/
+
+        
+# @csrf_exempt
+# def fun1(request,pk):
+#     try:
+#         demo=stud.objects.get(pk=pk)
+#         # print('helo')
+#     except:
+#         return HttpResponse("Invalid")
+#     if request.method=='GET':
+#         s=studModelSerializer(demo)
+#         return JsonResponse(s.data)
+#     elif request.method=='PUT':
+#         d=JSONParser().parse(request)
+#         s=studModelSerializer(demo,data=d)
+#         if s.is_valid():
+#             s.save()
+#             return JsonResponse(s.data)
+#         else:
+#             return JsonResponse(s.errors)       
+#     elif request.method=='DELETE':
+#         demo.delete()
+#         return HttpResponse("Deleted")
+
+
+
+#/-------------------------------------------------------------------------------------------------------\
+#    ***************************************** API_VIEW  GET,POST ********************************
+#\-------------------------------------------------------------------------------------------------------/
+
+
+
+# @api_view(['GET','POST'])
+# def fun1(req):
+#     if req.method == 'GET':
+#         d = stud.objects.all()
+#         s = studModelSerializer(d, many=True)
+#         return Response(s.data)
+
+#     elif req.method == 'POST':
+#         s = studModelSerializer(data=req.data)
+#         if s.is_valid():
+#             s.save()
+#             return JsonResponse(s.data, status=status.HTTP_201_CREATED)
+#         else:
+#             return JsonResponse(s.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+#/---------------------------------------------------------------------------------------------------------\
+#   ***************************************** API_VIEW  GET,PUT,DELETE ********************************
+#\---------------------------------------------------------------------------------------------------------/
+
+
+# @api_view(['GET','PUT','DELETE'])
+# def fun1(req,pk):
+#     try:
+#         demo=stud.objects.get(pk=pk)
+#     except:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#     if req.method == 'GET':
+#         s = studModelSerializer(demo)
+#         return Response(s.data)
+#     elif req.method == 'PUT':
+#         s = studModelSerializer(demo,data=req.data)
+#         if s.is_valid():
+#             s.save()
+#             return Response(s.data)
+#         else:
+#             return Response(status=status.HTTP_400_BAD_REQUEST)
+#     elif req.method == 'DELETE':
+#         demo.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
     
+    
+# /-------------------------------------------------------------------------------------------\
+#     **************************************  API_VIEW CLASS  *****************************
+# \-------------------------------------------------------------------------------------------/
+
+class fun1(APIView):
+    def get(self,request):
+       demo=stud.objects.all()
+       s=studModelSerializer(demo,many=True)
+       return Response(s.data)
+    def post(self,request):
+        s=studModelSerializer(data=request.data)
+        if s.is_valid():
+            s.save()
+            return JsonResponse(s.data,status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(status=status.HTTP_400_BAD_REQUEST)
+
 # Create your views here.
